9e956b3f36605d30c7e1863f73b56cf003b46e66
onkarsherkar <onkar.sherkar@gmail.com>
2025-11-28 22:18:55 +0530

    Added Player component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import Player from './components/Player'
+
 
 function App() {
   
@@ -6,14 +8,23 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players">
-          <li>
-            <span className="player-name">Player 1</span>
-            <span className="player-symbol">X</span>
-          </li>
-          <li>
-            <span className="player-name">Player2</span>
-            <span className="player-symbol">O</span>
-          </li>
+
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O"/>
+          {/* <li>
+            <span classname="player">
+              <span className="player-name">Player 1</span>
+              <span className="player-symbol">X</span>
+            </span>
+            <button>Edit</button>
+          </li> */}
+          {/* <li>
+            <span className="player">
+              <span className="player-name">Player2</span>
+              <span className="player-symbol">O</span>
+            </span>
+            <button>Edit</button>
+          </li> */}
         </ol>
       </div>
     </main>
